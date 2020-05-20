6a71aa506a3fec6177f7e82bb565b27e41dfa8de
physio <physio@users.noreply.github.com>
2020-05-20 23:02:02 +0200

    added composer and packages

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,3 +12,9 @@ apt-get -y install php7.4-bcmath
 apt-get -y install php7.4-imagick
 
 update-alternatives --set php /usr/bin/php7.4
+
+# install compose
+apt-get -y install composer
+
+# install packakages
+composer install
