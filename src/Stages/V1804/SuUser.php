41b18eaed972f16142d6cd13afd271e89d3861e4
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-05-21 07:17:44 +0000

    Fix sudoer catch

--- src/Stages/V1804/SuUser.php
+++ src/Stages/V1804/SuUser.php
@@ -15,49 +15,49 @@ class SuUser extends StageBase implements Stage
 {
     public function run(array $env = null): bool
     {
-        //try {
-        $user = APP_PANEL_USER;
-        $home = "/home/{$user}";
-        $password = $env['password'];
+        try {
+            $user = APP_PANEL_USER;
+            $home = "/home/{$user}";
+            $password = $env['password'];
 
-        if (!sudo()) {
-            $this->internal = 'This user is not an superuser';
+            if (!sudo()) {
+                $this->internal = 'This user is not an superuser';
+
+                return false;
+            }
+
+            $exists = $this->runner->run(['id', '-u', $user])->success();
+
+            if ($exists) {
+                $this->command(['userdel', $user]);
+            }
+
+            $this->command(['useradd', '-d', $home, $user, '-s', '/bin/bash', '-p', $this->encodePassword($password)]);
+
+            if (!File::exists($home)) {
+                File::makeDirectory($home);
+            }
+
+            $this->command(['chown', "{$user}:{$user}", $home]);
+
+            $conf = $this->template('sudoer.conf');
+
+            $written = File::put('/etc/sudoers.d/' . APP_PANEL_USER, str_replace('{USERNAME}', APP_PANEL_USER, $conf));
+
+            if (!$written) {
+                $this->internal = 'Unable to write sudoer configuration';
+
+                return false;
+            }
+
+            return true;
+
+        } catch (Exception $e) {
+
+            Log::error($e->getMessage());
 
             return false;
         }
-
-        $exists = $this->runner->run(['id', '-u', $user])->success();
-
-        if ($exists) {
-            $this->command(['userdel', $user]);
-        }
-
-        $this->command(['useradd', '-d', $home, $user, '-s', '/bin/bash', '-p', $this->encodePassword($password)]);
-
-        if (!File::exists($home)) {
-            File::makeDirectory($home);
-        }
-
-        $this->command(['chown', "{$user}:{$user}", $home]);
-
-        $conf = $this->template('sudoer.conf');
-
-        $written = File::put('/etc/sudoers.d/' . APP_PANEL_USER, str_replace('{USERNAME}', APP_PANEL_USER, $conf));
-
-        if (!$written) {
-            $this->internal = 'Unable to write sudoer configuration';
-
-            return false;
-        }
-
-        return true;
-
-        // } catch (Exception $e) {
-
-        //   Log::error($e->getMessage());
-
-        // return false;
-        //}
     }
 
     private function encodePassword($password): string
