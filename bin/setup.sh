4143a695416da0b06ca806af60abdbf60209464c
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-05-27 07:44:23 +0000

    Fix setup.sh release url

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,7 +13,7 @@ apt-get -y install php7.4-fpm php7.4-common php7.4-mbstring php7.4-mysql php7.4-
 update-alternatives --set php /usr/bin/php7.4 >> installer.log 2>&1
 
 echo "Downloading installer..."
-wget -O installer https://github.com/sculptor-devops/installer/raw/master/builds/installer >> installer.log 2>&1
+wget -O installer https://github.com/sculptor-devops/installer/releases/latest/download/installer >> installer.log 2>&1
 chmod +x installer >> installer.log 2>&1
 
 echo "Running installer..."
