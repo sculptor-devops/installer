42a6fb11cc7bdc3c31ba96097f27b9e9a944d4da
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-05-27 07:57:54 +0000

    Hide migration commands

--- config/commands.php
+++ config/commands.php
@@ -66,6 +66,22 @@ return [
         Illuminate\Console\Scheduling\ScheduleRunCommand::class,
         Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
         Illuminate\Foundation\Console\VendorPublishCommand::class,
+       
+	Illuminate\Database\Console\WipeCommand::class,
+	Illuminate\Database\Console\Seeds\SeedCommand::class,
+
+	Illuminate\Database\Console\Seeds\SeederMakeCommand::class,
+
+	Illuminate\Database\Console\Factories\FactoryMakeCommand::class,
+
+	Illuminate\Database\Console\Migrations\FreshCommand::class,
+	Illuminate\Database\Console\Migrations\InstallCommand::class,
+	Illuminate\Database\Console\Migrations\MigrateCommand::class,
+	Illuminate\Database\Console\Migrations\MigrateMakeCommand::class,
+	Illuminate\Database\Console\Migrations\ResetCommand::class,
+	Illuminate\Database\Console\Migrations\RefreshCommand::class,
+	Illuminate\Database\Console\Migrations\RollbackCommand::class,
+	Illuminate\Database\Console\Migrations\StatusCommand::class
     ],
 
     /*
