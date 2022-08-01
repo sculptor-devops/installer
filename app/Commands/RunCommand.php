38138f1ba2abd075300abac0eaa114340d48e460
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2022-08-01 21:20:47 +0200

    Fixed run command wiht test message]

--- app/Commands/RunCommand.php
+++ app/Commands/RunCommand.php
@@ -34,20 +34,6 @@ class RunCommand extends Command
      */
     public function handle(Stages $stages): int
     {
-        render(<<<'HTML'
-    <div class="mx-2 my-1">
-        <div class="flex space-x-1">
-            <span class="flex-1 truncate">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt illo et nisi omnis porro at, mollitia harum quas esse, aperiam dolorem ab recusandae fugiat nesciunt doloribus rem eaque nostrum itaque.</span>
-            <span class="text-green">DONE</span>
-        </div>
-    </div>
-HTML);
-
-
-        return 0;
-
-
-
         $this->preamble();
 
         if (!$this->deprecated($stages->deprecated())) {
